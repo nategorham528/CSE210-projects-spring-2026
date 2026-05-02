9b3d61cf3226cdf640083aad3309d9c432d9727c
Nate Gorham <nategorham528@gmail.com>
2026-05-02 15:03:29 -0600

    finished code

---
+++ In_Class_Work/class_week_2.cs
@@ -0,0 +1,17 @@
+Console.WriteLine("Hello.");
+Console.ReadLine();
+
+/* This is a comment
+   that spans across
+   multiple lines */
+
+// This is a comment for a single line.
+
+/*  for()          - 0-> n
+    while()        - 0-> ?
+    do{}while();  - 1-> ?
+    Foreach()      - collections(ie lists, dictionaries)
+*/
+
+/*  IPO = Input, Process, Output
+    */
