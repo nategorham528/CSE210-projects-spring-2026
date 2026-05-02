da961883e305e434d1c036c3d6428b01fd4730b1
nategorham528 <nategorham528@gmail.com>
2026-05-02 16:38:42 -0600

    Update Program.cs

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,42 @@
 using System;
+using System.Formats.Asn1;
+using System.Globalization;
+using System.Transactions;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        int num = 1000;
+        int sum = 0;
+        float average = 0;
+        int largest = 0;
+        int smallest = 1000000000;
+        List<int> numbers = new List<int>();
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        while(num != 0){
+            if(num != 0){
+                Console.Write("Enter Number: ");
+                string enter = Console.ReadLine();
+                num = int.Parse(enter);
+                numbers.Add(num);
+            }
+        }
+
+        foreach(int number in numbers){
+            sum += number;
+            if(number > largest){
+                largest = number;
+            }
+            else if(number < smallest && number > 0){
+                smallest = number;
+            }
+        } 
+        average = (float)sum / numbers.Count;
+
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest positive number: {largest}");
+        Console.WriteLine($"The smallest positive number is: {smallest}");
     }
 }
