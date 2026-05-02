a0b74c86ae758d2c52f1ecfd6dba97d080da9ecc
nategorham528 <nategorham528@gmail.com>
2026-05-02 15:42:08 -0600

    Prep_3_fin.cs

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -3,7 +3,28 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep3 World!");
-    }
+    {        
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 100);
+        string match = "no";
+
+        do{
+            Console.Write("What is your guess? ");
+            string num = Console.ReadLine();
+            int guess = int.Parse(num);
+            
+            if(guess == number){
+                Console.WriteLine("You guessed it!");
+                match = "yes";
+            }
+            else if(guess > number){
+                Console.WriteLine("Lower");
+
+            }
+            else{
+                Console.WriteLine("Higher");
+            }
+          } while(match == "no");
+    } 
+
 }
