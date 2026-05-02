7e5a24fed5a1e1330790f0562f37154f26883004
nategorham528 <nategorham528@gmail.com>
2026-05-02 15:00:17 -0600

    Update Program.cs

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade? ");
+        string score = Console.ReadLine();
+        int grade = int.Parse(score);
+        string letter = "";
+        string PassFail = "";
+
+        if(grade >= 90){
+            letter = "A";
+        }
+        else if(grade >=80){
+            letter = "B";
+        }
+        else if(grade >=70){
+            letter = "C";
+        }
+        else if(grade >=60){
+            letter = "D";
+        }
+        else{
+            letter = "F";
+        }
+
+        if(grade >= 70){
+            PassFail = "you passed the class!";
+        }
+
+        else{
+            PassFail = "you didn't pass. Better luck next time!";
+        }
+        
+        Console.WriteLine($"Your final grade is a/an {letter} and {PassFail}");
     }
 }
